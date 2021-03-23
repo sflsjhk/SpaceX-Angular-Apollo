02c7143e39139d5d51c0ac7755c638f62b65a566
Hekang Jia <48497574+sflsjhk@users.noreply.github.com>
2021-03-23 13:22:39 -0400

    update dependency

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', async () => {
     await page.navigateTo();
-    expect(await page.getTitleText()).toEqual('spacex-angular-apollo app is running!');
+    expect(await page.getTitleText()).toEqual('plasmaAngular app is running!');
   });
 
   afterEach(async () => {
--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/spacex-angular-apollo'),
+      dir: require('path').join(__dirname, './coverage/plasmaAngular'),
       subdir: '.',
       reporters: [
         { type: 'html' },
