54b21e35b490e59805e21f4c31e5a04a7907bd9d
Hekang Jia <48497574+sflsjhk@users.noreply.github.com>
2021-03-23 13:11:54 -0400

    launchdetails

--- src/app/components/launchlist/launchlist.component.ts
+++ src/app/components/launchlist/launchlist.component.ts
@@ -1,15 +1,1 @@
-import { Component, OnInit } from '@angular/core';
 
-@Component({
-  selector: 'app-launchlist',
-  templateUrl: './launchlist.component.html',
-  styleUrls: ['./launchlist.component.sass']
-})
-export class LaunchlistComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
-}
